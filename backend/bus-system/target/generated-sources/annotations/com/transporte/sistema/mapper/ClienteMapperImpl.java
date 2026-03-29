1e5873b26440d2cb6cb069c47d41d9cd0396a57f
Leonel785 <camposhuaman85@gmail.com>
2026-03-29 15:19:19 -0500

    admind panel login actual

--- backend/bus-system/target/generated-sources/annotations/com/transporte/sistema/mapper/ClienteMapperImpl.java
+++ backend/bus-system/target/generated-sources/annotations/com/transporte/sistema/mapper/ClienteMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-15T19:53:06-0500",
+    date = "2026-03-29T15:17:40-0500",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
